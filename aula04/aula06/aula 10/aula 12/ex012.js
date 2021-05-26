702458806ae19b47830f4a5f5c09674fa10ab885
ERLISON-dev <83833883+ERLISON-dev@users.noreply.github.com>
2021-05-26 18:59:11 -0300

    att de condiçoes js

---
+++ aula04/aula06/aula 10/aula 12/ex012.js
@@ -0,0 +1,15 @@
+var idade = 15
+
+if (idade < 16) { 
+    console.log('Não Vota')
+}
+else { 
+   if (idade < 18) { 
+       console.log('Voto opcional')
+
+   }
+   else { 
+       console.log('Voto Obrigatorio')
+   }
+    
+}
